6a7b5f477eb9921a514c630c15dec16835e731da
tsteciuk <90786926+tsteciuk@users.noreply.github.com>
2025-07-11 11:29:54 -0400

    Create gudonov.py

---
+++ gudonov.py
@@ -0,0 +1,12 @@
+import matplotlib
+matplotlib.use('Agg')
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Set initial data by modifying this function
+def InitialData(input):
+  if(input<0):
+    return u_l;
+  return u_r;
+
+#Main
